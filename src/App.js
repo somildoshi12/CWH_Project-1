c7bbb19a6be1f4fe5143b6344921d83fde74aac4
somildoshi12 <somildoshi12@gmail.com>
2021-10-20 17:56:49 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import React, { useState } from 'react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'black';
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <Navbar title="TextUtiles" mode={mode} toggleMode={toggleMode} />
+
+        <div className="container">
+
+          <Switch>
+
+            <Route exact path="/about">
+              <About />
+            </Route>
+
+            <Route exact path="/">
+              <TextForm heading="Enter Text to Analyse" mode={mode} />
+            </Route>
+
+          </Switch>
+
+        </div>
+
+      </Router>
+
     </div>
   );
 }
---
+++ src/Components/TextForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState('');
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleCopy = () => {
+        let txt = document.getElementById('my-box');
+        txt.select();
+        navigator.clipboard.writeText(txt.value);
+    }
+
+    return (
+        <div>
+            <div className="container">
+
+                <h2 style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{props.heading}</h2>
+
+                <div className="mb-3">
+
+                    <textarea className="form-control" id="my-box" rows="10" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black', border: props.mode === 'dark' ? '2px solid white' : '2px solid black' }}></textarea>
+
+                </div>
+
+                <button className="btn btn-primary my-2" onClick={handleUpClick} style={{ color: props.mode === 'dark' ? 'white' : 'black' }}><b>To UpperCase</b></button>
+
+                <button className="btn btn-primary my-2 mx-3" onClick={handleLowClick} style={{ color: props.mode === 'dark' ? 'white' : 'black' }}><b>To lowerCase</b></button>
+
+                <button className="btn btn-primary my-2" onClick={handleCopy} style={{ color: props.mode === 'dark' ? 'white' : 'black' }}><b>Copy Text</b></button>
+
+                <hr style={{ color: props.mode === 'dark' ? 'white' : 'black' }} />
+
+            </div>
+
+            <div className="container">
+
+                <h3 style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>Text Summary</h3>
+
+                <p style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{text.split(" ").length} words & {text.length} characters</p>
+
+                <hr style={{ color: props.mode === 'dark' ? 'white' : 'black' }} />
+
+            </div>
+
+            <div className="container">
+
+                <h3 style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>Preview</h3>
+
+                <p style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>{text}</p>
+
+            </div>
+
+        </div>
+
+    )
+}
